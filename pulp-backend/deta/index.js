b071a79a8b8fe1c73d8d4f8806fa0a09dbc21c91
bhavik8455 <solankibhavik92@gmail.com>
2024-03-17 20:58:47 +0530

    updated one cache logic

--- pulp-backend/deta/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const { Deta } = require("deta");
-const deta = Deta("d0VWxU4YRx3A_4Aeo76BPSPS9eEmvjFHKpDThNEEJ17mt");
-const base = deta.Base("pulp");
-const drive = deta.Drive("pulp");
-
-module.exports = { base, drive };
